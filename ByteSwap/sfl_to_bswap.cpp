5f8dfc06ebd070800bb7955ede9f2a3ad2636381
Laishzh <laishengzhang@gmail.com>
2014-04-28 13:41:55 +0800

    Add isByteSwap64() Function

--- ByteSwap/sfl_to_bswap.cpp
+++ ByteSwap/sfl_to_bswap.cpp
@@ -4,14 +4,59 @@
 #include "llvm/Support/raw_ostream.h"
 #include "llvm/Support/Debug.h"
 
-static bool isByteSwap(VectorType &LHS, VectorType &RHS,
-                       SmallVector<int, 16> &Masks)
-{
-
-    return false;
-}
 
 using namespace llvm;
+static bool isByteSwap64(ShuffleVectorInst &SI, SmallVector<int, 16>&RefMasks)
+{
+    RefMasks.clear();
+    unsigned VWidth = cast<VectorType>(SI.getType())->getNumElements();
+    VectorType *LHS = cast<VectorType>(SI.getOperand(0)->getType());
+    VectorType *RHS = cast<VectorType>(SI.getOperand(1)->getType());
+
+    IntegerType *IT = dyn_cast<IntegerType>(LHS->getElementType());
+    if (IT == nullptr
+        || ! IT->isIntegerTy(8)
+        || VWidth % 8 != 0) {
+        return false;
+    }
+
+    SmallVector<int, 16> Masks(SI.getShuffleMask());
+    bool isByteSwap = true;
+
+    for (unsigned i = 0; i < VWidth / 8; ++i) {
+        unsigned base = Masks[i * 8];
+        if (base % 8 != 7) {
+            isByteSwap = false;
+            break;
+        }
+
+        for (unsigned j = 1; j < 8; ++j) {
+            if (base - Masks[i * 8 + j] != j) {
+                isByteSwap = false;
+                break;
+            }
+        }
+
+        if (isByteSwap) {
+            RefMasks.push_back(base / 8);
+        } else {
+            break;
+        }
+    }
+
+    if (!isByteSwap) {
+        RefMasks.clear();
+    }
+
+    return isByteSwap;
+}
+
+static void replaceShuffleVectorWithByteSwap64()
+{
+
+    return ;
+}
+
 namespace {
     class ShufflevectorToByteSwapPass:public llvm::BasicBlockPass {
     public:
@@ -27,24 +72,34 @@ namespace {
                     errs() << "ShuffleVector\n";
                     Value *LHS = si->getOperand(0);
 
-                    unsigned LHSWidth = cast<VectorType>(LHS->getType())->getNumElements();
-                    unsigned LHSBitWidth = cast<VectorType>(LHS->getType())->getBitWidth();
-                    errs() << "LHSWidth: " << LHSWidth << "; " << "LHSBitWidth: " << LHSBitWidth << "\n";
+                    unsigned LHSWidth
+                        = cast<VectorType>(LHS->getType())->getNumElements();
+                    unsigned LHSBitWidth
+                        = cast<VectorType>(LHS->getType())->getBitWidth();
+                    errs() << "LHSWidth: " << LHSWidth << "; "
+                           << "LHSBitWidth: " << LHSBitWidth << "\n";
 
-                    Type *ElementType = cast<VectorType>(LHS->getType())->getElementType();
+                    Type *ElementType
+                        = cast<VectorType>(LHS->getType())->getElementType();
                     unsigned ElementWidth = 0;
 
                     if (IntegerType *it = dyn_cast<IntegerType>(ElementType)) {
                         ElementWidth = it->getBitWidth();
-                        errs() << "It's a IntegerType and it has " << ElementWidth << " Bits." << "\n";
+                        errs() << "It's a IntegerType and it has "
+                               << ElementWidth << " Bits." << "\n";
                     }
-                    errs() << cast<VectorType>(LHS->getType())->getElementType()->isIntegerTy(8) << "\n";
-                    errs() << cast<VectorType>(LHS->getType())->getElementType()->isIntegerTy(16) << "\n";
+                    errs() << cast<VectorType>(LHS->getType())
+                        ->getElementType()->isIntegerTy(8) << "\n";
+                    errs() << cast<VectorType>(LHS->getType())
+                        ->getElementType()->isIntegerTy(16) << "\n";
 
 
-                    unsigned VWidth = cast<VectorType>(si->getType())->getNumElements();
-                    unsigned VBitWidth = cast<VectorType>(si->getType())->getBitWidth();
-                    errs() << "VWidth: " << VWidth << "; " << "VBitWidth: " << VBitWidth << "\n";
+                    unsigned VWidth
+                        = cast<VectorType>(si->getType())->getNumElements();
+                    unsigned VBitWidth
+                        = cast<VectorType>(si->getType())->getBitWidth();
+                    errs() << "VWidth: " << VWidth << "; "
+                           << "VBitWidth: " << VBitWidth << "\n";
 
                     si->print(errs());
                     errs() << "\n";
@@ -60,49 +115,22 @@ namespace {
                     }
                     errs() << "\n";
 
-                    IntegerType *it = dyn_cast<IntegerType>(ElementType);
-                    if (VWidth > 0
-                        && VWidth % 8 == 0
-                        && it != nullptr
-                        && it->isIntegerTy(8)) {
-                        //Check if ShuffleVector can be translated into
-                        //several llvm.bswap.i64 operations.
-                        SmallVector<int, 8> checkmasks;
-                        bool checkflag = true;
-                        for (unsigned i = 0; i < VWidth / 8; ++i) {
-                            unsigned base = masks[i * 8];
-                            if (base % 8 != 7) {
-                                checkflag = false;
-                                errs() << "Break at Base: " << base << "\n";
-                                break;
-                            }
-                            for (unsigned j = 1; j < 8; ++j) {
-                                if (base - masks[i*8 + j] != j) {
-                                    checkflag = false;
-                                    errs() << "Break at: " << i << ":" << j << "\n";
-                                    break;
-                                }
-                            }
-                            if (!checkflag) {
-                                break;
-                            } else {
-                                checkmasks.push_back(base / 8);
-                            }
+                    SmallVector<int, 16> RefMasks;
+
+                    bool isBswap = isByteSwap64(*si, RefMasks);
+                    if (isBswap) {
+                        errs() << "Shufflevector can be translated "
+                            "into ByteSwap.\n";
+                        for (unsigned i = 0; i < RefMasks.size(); ++i) {
+                            errs() << RefMasks[i] << " ";
                         }
-
-                        if (checkflag) {
-                            errs() << "Can be translated.\n";
-                            for (unsigned i = 0; i < checkmasks.size(); ++i) {
-                                errs() << checkmasks[i] << " ";
-                            }
-                            errs() << "\n";
-                        } else {
-                            errs() << "Cannot be translated.\n";
-                            //TODO:change this
-                        }
-
-
+                        errs() << "\n";
+                    } else {
+                        errs() << "Shufflevector cannot be translated "
+                            "into ByteSwap.\n";
                     }
+
+
 
                 }
                     //DEBUG(errs() << "I am here!\n");
@@ -116,4 +144,5 @@ namespace {
     char ShufflevectorToByteSwapPass::ID = 0;
 }
 
-static RegisterPass<ShufflevectorToByteSwapPass> X("SFLtoBSwap", "Shufflevector to ByteSwap Pass", false, false);
+static RegisterPass<ShufflevectorToByteSwapPass>
+    X("SFLtoBSwap", "Shufflevector to ByteSwap Pass", false, false);
